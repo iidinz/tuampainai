1549687763efe0247003fd7227542340d33c0ec3
iidinz <pongponkub1@gmail.com>
2026-05-26 17:30:46 +0700

    Fix satellite basemap style

--- lib/map/style.ts
+++ lib/map/style.ts
@@ -1,2 +1,24 @@
+import type { StyleSpecification } from 'maplibre-gl';
+
 export const LIGHT_STYLE = 'https://tiles.openfreemap.org/styles/positron';
-export const SATELLITE_STYLE = 'https://tiles.openfreemap.org/styles/positron';
+
+export const SATELLITE_STYLE: StyleSpecification = {
+  version: 8,
+  sources: {
+    satellite: {
+      type: 'raster',
+      tiles: [
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      ],
+      tileSize: 256,
+      attribution: 'Tiles © Esri',
+    },
+  },
+  layers: [
+    {
+      id: 'satellite',
+      type: 'raster',
+      source: 'satellite',
+    },
+  ],
+};
